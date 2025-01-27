9ad8c2233a20f9728d86114c105e8531c8555e14
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2025-01-27 20:37:13 +0530

    feat: add bullmq setup, but there is an issue with QueueScheduler import

---
+++ src/crawler/queue/queueManager.ts
@@ -0,0 +1,33 @@
+import {Queue, QueueScheduler} from 'bullmq';
+import {createClient} from 'redis';
+
+//redis
+const redisClient = createClient({ url: 'redis://localhost:6379' });
+
+(async () => {
+    try {
+        await redisClient.connect();
+        console.log('Connected to Redis');
+    } catch (error) {
+        console.error('Error connecting to Redis:', error);
+        process.exit(1); // Exit the process if Redis connection fails
+    }
+})();
+
+//URL queue for managing crawling jobs
+
+const urlQueue = new Queue('urlQueue', {
+    connection: {
+        client: redisClient,
+    },
+});
+
+
+//Scheduler for urlQueue
+try {
+    new QueueScheduler('urlQueue', { connection: { client: redisClient } });
+    console.log('QueueScheduler for urlQueue initialized');
+} catch (error) {
+    console.error('Error initializing QueueScheduler:', error);
+}
+
